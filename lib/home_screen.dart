bc8be48b783b6390b797a102bb92d550119406b9
iamdumonde <martinaboumonde@gmail.com>
2024-10-28 18:07:26 +0100

    search button home_screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dictio_app_test/result_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -20,6 +21,38 @@ class HomeScreen extends StatelessWidget {
                 "assets/dictionaryapp.png",
                 fit: BoxFit.cover,
                 height: MediaQuery.of(context).size.height / 3.5,
+              ),
+              SizedBox(height: 50),
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: "Enter a Word",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 50),
+              Material(
+                borderRadius: BorderRadius.circular(10),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  height: 55,
+                  color: Colors.indigo,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ResultScreen(),
+                        ));
+                  },
+                  child: Text(
+                    "Search",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
